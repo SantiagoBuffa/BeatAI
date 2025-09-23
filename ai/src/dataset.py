3e83cf4d22b4af50f8681c385c3baea94a576ef6
Santiago Buffa <buffa_snt@yahoo.com>
2025-09-23 15:33:32 -0300

    auxiliar carga de dataset

--- ai/src/dataset.py
+++ ai/src/dataset.py
@@ -1,13 +1,24 @@
-# loads and preprocesses the dataset
-from google.colab import drive
-import os
+#loads and preprosseses the Dataset
 
 
-drive.mount('/content/drive')
-dataset_path = '/content/drive/MyDrive/IA-Proyecto/dataset'  # cambialo por tu ruta real
+#PEGAR EN EL COLAB ANTES DE CORRER
+#drive.mount('/content/drive')
+#dataset_path = '/content/drive/MyDrive/IA-Proyecto/dataset'  # cambialo por tu ruta real
 
-if os.path.exists(dataset_path):
-    print(f"✅ La carpeta existe: {dataset_path}")
-else:
-    print(f"❌ La carpeta no se encuentra. Revisá la ruta: {dataset_path}")
+#if os.path.exists(dataset_path):
+    #print(f"✅ La carpeta existe: {dataset_path}")
+#else:
+    #print(f"❌ La carpeta no se encuentra. Revisá la ruta: {dataset_path}")
     
+    
+import os
+import tensorflow as tf
+
+def load_dataset(dataset_path, img_size(128,128), batch_size = 16):
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+    # Data augmentation para entrenamiento
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        validation_split=0.2  # will separate 20% of the data to use in validation
+    )
