92ea650284a52b9ada0415d63d9a152dd5db80a1
Santiago Buffa <buffa_snt@yahoo.com>
2025-09-23 14:59:48 -0300

    prueba de colab

--- ai/src/dataset.py
+++ ai/src/dataset.py
@@ -1,1 +1,13 @@
 # loads and preprocesses the dataset
+from google.colab import drive
+import os
+
+
+drive.mount('/content/drive')
+dataset_path = '/content/drive/MyDrive/IA-Proyecto/dataset'  # cambialo por tu ruta real
+
+if os.path.exists(dataset_path):
+    print(f"✅ La carpeta existe: {dataset_path}")
+else:
+    print(f"❌ La carpeta no se encuentra. Revisá la ruta: {dataset_path}")
+    
