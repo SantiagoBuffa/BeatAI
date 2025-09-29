a95aeb6b1edfd8242fa8f7082520fbcb5621c157
Santiago Buffa <buffa_snt@yahoo.com>
2025-09-29 19:14:23 -0300

    manipulacion del dataset aux

--- ai/src/dataset.py
+++ ai/src/dataset.py
@@ -8,8 +8,39 @@ import tensorflow as tf
 def load_dataset(dataset_path, img_size(128,128), batch_size = 16):
     from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-    # Data augmentation para entrenamiento
+    # Data augmentation 
     train_datagen = ImageDataGenerator(
-        rescale=1./255, 
-        validation_split=0.2  # will separate 20% of the data to use in validation
+        rescale=1./255,  #rescale
+        rotation_range=5,  # slight rotation
+        width_shift_range=0.05, # 5% horizontal shift
+        height_shift_range=0.05, # 5% vertical shift
+        zoom_range=0.05,   #slight zoom
+        shear_range=0.05,  #slight deformation
+        validation_split=0.15 # will use 15% data for validation
     )
+    
+    img_height, img_width = 256, 192  # keeping the proportions of the original images
+    batch_size = 16 
+    
+    # loads up the drive folder and gets them ready to train with
+    train_generator = train_datagen.flow_from_directory(
+        os.path.join(dataset_path, 'train'), # path
+        target_size=(img_height, img_width), 
+        color_mode = 'grayscale', 
+        batch_size=batch_size,
+        class_mode='categorical', # each image has a category (from its folder)
+        subset='training',
+        shuffle=True # mixes the images every epoch
+    )
+    
+    # repeat with the validation part of the dataset
+    validation_generator = train_datagen.flow_from_directory(
+        os.path.join(dataset_path, 'train'),
+        target_size=(img_height, img_width),
+        color_mode = 'grayscale', 
+        batch_size=batch_size,
+        class_mode='categorical',
+        subset='validation',
+        shuffle=True
+    )
+    
